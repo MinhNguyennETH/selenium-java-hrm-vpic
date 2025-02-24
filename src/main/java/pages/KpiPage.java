2fc0f25bf91eb92796ef3b1b5d534424f57dbec9
Minh Nguyen Van <150711695+MinhNguyennETH@users.noreply.github.com>
2025-02-24 11:21:16 +0700

    Update KpiPage.java

--- src/main/java/pages/KpiPage.java
+++ src/main/java/pages/KpiPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -20,17 +21,35 @@ public class KpiPage {
     @FindBy(xpath = "//span[normalize-space()='KPI cá nhân']")
     private WebElement kpiCaNhanButton;
 
+    //-------------TIEN DO THUC HIEN KPI CA NHAN----------------------------------------------------------
     @FindBy(xpath = "//span[contains(text(),'Tiến độ thực hiện KPI cá nhân')]")
     private WebElement tienDoKPIButton;
-
-    @FindBy(xpath = "//span[normalize-space()='KPI cá nhân theo Month']")
-    private WebElement kpiCaNhanTheoThangButton;
 
     @FindBy(xpath = "//div[contains(text(),'Tiến độ thực hiện KPI cá nhân')]")
     private WebElement tienDoKPITitle;
 
+
+    //----------------KPI CA NHAN THEO MONTH -------------------------------------------------------------
+    @FindBy(xpath = "//span[normalize-space()='KPI cá nhân theo Month']")
+    private WebElement kpiCaNhanTheoThangButton;
+
     @FindBy(xpath = "//div[contains(text(),'KPI cá nhân theo Month')]")
     private WebElement kpiCaNhanTheoThangTitle;
+
+    @FindBy(xpath = "//span[@id='select2-chosen-1']")
+    private WebElement SelectButton;
+
+    @FindBy(xpath = "//input[@id='s2id_autogen1_search']")
+    private WebElement inputSearch;
+
+    @FindBy(xpath = "//i[@class='view-details fa fa-pencil-square-o']")
+    private WebElement ViewKPI;
+
+    @FindBy(xpath = "//span[@id='ui-id-1']")
+    private WebElement Verify;
+
+
+    //-----------------------------------------------------------------------------------------------------
 
     public KpiPage(WebDriver driver) {
         this.driver = driver;
@@ -53,11 +72,15 @@ public class KpiPage {
         wait.until(ExpectedConditions.elementToBeClickable(quanLyKPIButton)).click();
         wait.until(ExpectedConditions.elementToBeClickable(kpiCaNhanButton)).click();
         wait.until(ExpectedConditions.elementToBeClickable(kpiCaNhanTheoThangButton)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(SelectButton)).click();
+        wait.until(ExpectedConditions.elementToBeClickable(inputSearch)).sendKeys("2024", Keys.ENTER);
+        
+        wait.until(ExpectedConditions.elementToBeClickable(ViewKPI)).click();
+
     }
 
 
-    public boolean isKPICaNhanTheoThangDisplayed() {
-
-        return wait.until(ExpectedConditions.visibilityOf(kpiCaNhanTheoThangTitle)).isDisplayed();
+    public boolean VerifyKpiCaNhanTheoMonth() {
+        return wait.until(ExpectedConditions.visibilityOf(Verify)).isDisplayed();
     }
 }
