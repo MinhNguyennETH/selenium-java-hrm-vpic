fa6bd3fc61400be1b34c399664c7730ceaae41d5
MinhNguyenETH <minhnguyen.eth@gmail.com>
2025-03-13 19:46:37 +0700

    fix basetest

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -3,15 +3,18 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import ultils.Config;
 
 public class BaseTest {
     protected WebDriver driver;
 
 
+    @Parameters("browser")
     @BeforeMethod
-    public void setup() {
-        driver = DriverFactory.getDriver();
+    public void setup(@Optional("chrome") String browser) {
+        driver = DriverFactory.getDriver(browser);
         driver.get(Config.url_Vpic);
     }
 
